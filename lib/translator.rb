f50ea42180f74b9c786e146c66b8bfc6d0fe2cf0
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-27 19:56:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,18 +1,22 @@
 require 'yaml'
 
-def load_library(file_path)
-  hash = YAML.load_file(file_path)
-  library_hash = {'get_meaning' => {}, 'get_emoticon' => {}} 
-  hash.each do |meaning, emoticons|
+
+def load_library(file_path) # need to add dis arg
+  unformatted_hash = YAML.load_file(file_path)  # meaning is a string, emoticons is an array of two emojis
+
+  library_hash = {'get_emoticon' => {}, 'get_meaning' => {}} # top level structure of hash, gotta do it this way
+
+  unformatted_hash.each do |meaning, emoticons| # meaning is a string, emoticons is an array of two emojis
     english = emoticons[0]
     japanese = emoticons[1]
-    
-    library_hash['get_meaning'][japanese] = meaning
-    library_hash['get_emoticon'][english] = meaning
 
-  end 
-  library_hash
-end 
+    library_hash['get_meaning'][japanese] = meaning # each key is japanese emoji that points to meaning
+    library_hash['get_emoticon'][english] = japanese # each key is an eng. emoji that points to the japanese one
+
+  end
+  library_hash  # we want to return a hash that has the right functionality
+end
+
 
 def get_japanese_emoticon
   # code goes here
