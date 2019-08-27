d0984b739da7105e83556a18925ef96acd25cb50
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-27 19:48:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library(file_path)
   hash = YAML.load_file(file_path)
   library_hash = {'get_meaning' => {}, 'get_emoticon' => {}} 
-  hash.each do |meaning
+  hash.each do |meaning, emoticon| 
  
 end
 
