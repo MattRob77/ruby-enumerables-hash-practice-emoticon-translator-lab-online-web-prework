f38b5f0010ea2d566d9b0befbd1d1d555ed542f8
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-27 20:05:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,10 +19,10 @@ end
 
 
 def get_japanese_emoticon(file_path, emoticon)
-  library = load_library(file_path)
-  result = library ['get_meaning'][emoticon]
-  result ? result : "not found"
-  
+   library = load_library(file_path) 
+  result = library['get_emoticon'][emoticon]
+
+  result ? result : "Sorry, that emoticon was not found"
 end
 
 
