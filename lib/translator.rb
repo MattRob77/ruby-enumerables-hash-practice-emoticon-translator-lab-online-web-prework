925ca26100670172a2d731c06fec195e3c6833b7
Matthew Robinson <matt_robinson1@yahoo.com>
2019-08-27 20:04:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   result = library ['get_meaning'][emoticon]
-  
+  result ? result : "not found"
   
 end
 
